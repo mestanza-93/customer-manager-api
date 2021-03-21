d0b78a1132c288dab9bd83e0b8f007ef34bb9e25
Pablo Mestanza <pablo93ml@gmail.com>
2021-03-21 20:36:25 +0100

    feat: new function with ApolloServer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const { ApolloServer } = require('apollo-server-cloud-functions');
 
 const constants = require("./constants")
 const extensions = ({ context }) => {
@@ -15,7 +16,7 @@ const graphqlSchema = require(constants.SCHEMAS_PATH + "/index");
 
 
 /**
- * Setup server
+ * Setup Express server
  */
 const app = express();
 app.use(cors({ origin: true }));
@@ -77,26 +78,31 @@ app.listen(constants.PORT, async () => {
 
 
 
+
+/**
+ * Setup Apollo Server
+ */
+const server = new ApolloServer({
+  schema: graphqlSchema,
+  context: ({ req, res }) => ({
+    headers: req.headers,
+    req,
+    res,
+  }),
+});
+
 /**
  * GraphQL handlers exports to functions
  */
-exports.graphqlHandler = graphqlHTTP({
-  context: { startTime: Date.now() },
-  schema: graphqlSchema,
-  extensions,
-  writeResponse: (result, res) => {
-    mongoose.disconnect();
 
-    res.set('Access-Control-Allow-Origin', "*");
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  
-    if (result.errors) {
-      res.send(500).json(sanitizeErrors(result))
-    } else {
-      res.send(200).json(result)
-    }
-  }
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']
+  },
 });
+
 
 exports.graphiqlHandler = graphqlHTTP({
   context: { startTime: Date.now() },
@@ -104,9 +110,6 @@ exports.graphiqlHandler = graphqlHTTP({
   schema: graphqlSchema,
   extensions,
   writeResponse: (result, res) => {
-    res.set('Access-Control-Allow-Origin', "*");
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  
     mongoose.disconnect();
   
     if (result.errors) {
